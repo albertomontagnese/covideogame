2aa500c2787510eb487dec3d227c353139f0f76a
Alberto <albertom@yelp.com>
2020-04-01 03:29:16 -0700

    added screenshot

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 
 cp -r landingPage/* covideogame/;
 cp Assets/Resources/favicon.png covideogame/;
+cp Assets/Resources/screenshot.png covideogame/;
 aws s3 sync ~/Desktop/covideogame/covideogame s3://covideogame/;
