d523effcf13d104921b66330f2c63a768d8dae06
Alberto <albertom@yelp.com>
2020-04-01 03:23:56 -0700

    added better landing page

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 
-cp index.html covideogame/index.html;
+cp -r landingPage/* covideogame/;
 cp Assets/Resources/favicon.png covideogame/;
 aws s3 sync ~/Desktop/covideogame/covideogame s3://covideogame/;
